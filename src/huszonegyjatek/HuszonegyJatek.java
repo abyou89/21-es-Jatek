9c984d167061bbbbe0bd7a6949c79baf557607ab
younes <younes.abdalla@gmail.com>
2024-06-01 13:28:47 +0200

    project letrejott

---
+++ src/huszonegyjatek/HuszonegyJatek.java
@@ -0,0 +1,26 @@
+
+package huszonegyjatek;
+
+
+public class HuszonegyJatek {
+    public static int[] j = new int[2],g = new int[2];
+
+    public static void main(String[] args) {
+        feladat();
+    
+    
+    }
+
+    private static void feladat() {
+    tombFeltoltes();   
+    
+    }
+
+    private static void tombFeltoltes() {
+        for (int i = 0; i < 1; i++) {
+            
+        }
+    
+    }
+    
+}
