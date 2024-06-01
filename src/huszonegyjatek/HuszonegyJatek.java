9793e5aef7ca23c4fba433dcda31c4a1196f195f
younes <younes.abdalla@gmail.com>
2024-06-01 13:58:49 +0200

    lapok kiosztasa és osszegzese

--- src/huszonegyjatek/HuszonegyJatek.java
+++ src/huszonegyjatek/HuszonegyJatek.java
@@ -3,7 +3,7 @@ package huszonegyjatek;
 
 
 public class HuszonegyJatek {
-    public static int[] j = new int[2],g = new int[2];
+    public static int[] jatekos = new int[2],gep = new int[2];
 
     public static void main(String[] args) {
         feladat();
@@ -14,6 +14,7 @@ public class HuszonegyJatek {
     private static void feladat() {
     tombFeltoltes();   
     tombKiir();
+    bekeres();
     }
 
     private static void tombFeltoltes() {
@@ -22,13 +23,16 @@ public class HuszonegyJatek {
     }
     private static void tombKiir() {
         tombKiirJatekos();
+        osszegzesJatekos();
+        tombKiirGep();
+        osszegzesGep();
     }
 
     private static void tombFeltoltesJatekos() {
         int felso = 11;
         int also = 2;
-        for (int i = 0; i < j.length; i++) {
-            j[i] = (int)(Math.random() * (felso - also + 1)) + also;
+        for (int i = 0; i < jatekos.length; i++) {
+            jatekos[i] = (int)(Math.random() * (felso - also + 1)) + also;
         }
     
     }
@@ -36,17 +40,45 @@ public class HuszonegyJatek {
     private static void tombFeltoltesGep() {
        int felso = 11;
         int also = 2;
-        for (int i = 0; i < g.length; i++) {
-            g[i] = (int)(Math.random() * (felso - also + 1)) + also;
+        for (int i = 0; i < gep.length; i++) {
+            gep[i] = (int)(Math.random() * (felso - also + 1)) + also;
         } 
     }
 
     private static void tombKiirJatekos() {
-        for (int i = 0; i < j.length; i++) {
-            System.out.print(j[i] + " ");
+        System.out.print("A jatekos lapjai: ");
+        for (int i = 0; i < jatekos.length; i++) {
+            System.out.print(jatekos[i] + " ");
         }
         System.out.println("");
     
+    }
+     private static void osszegzesJatekos() {
+         int osszeg = 0;
+         for (int i = 0; i < jatekos.length; i++) {
+             osszeg += jatekos[i];
+         }
+         System.out.println("A jatekos lapjainak osszege: " + osszeg);
+     }
+     
+    private static void tombKiirGep() {
+        System.out.print("A gep lapjai: ");
+        for (int i = 0; i < gep.length; i++) {
+            System.out.print(gep[i] + " ");
+        }
+        System.out.println("");
+    }
+    private static void osszegzesGep() {
+        int osszeg = 0;
+         for (int i = 0; i < gep.length; i++) {
+             osszeg += gep[i];
+         }
+         System.out.println("A gep lapjainak osszege: " + osszeg);
+    
+    }
+    
+    private static void bekeres() {
+        
     }
     
 }
