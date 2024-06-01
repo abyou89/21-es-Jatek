73788b9d967c8d18660bae52a57b0ef192524958
younes <younes.abdalla@gmail.com>
2024-06-01 13:41:51 +0200

    tomb kiiras

--- src/huszonegyjatek/HuszonegyJatek.java
+++ src/huszonegyjatek/HuszonegyJatek.java
@@ -13,13 +13,39 @@ public class HuszonegyJatek {
 
     private static void feladat() {
     tombFeltoltes();   
-    
+    tombKiir();
     }
 
     private static void tombFeltoltes() {
-        for (int i = 0; i < 1; i++) {
-            
+        tombFeltoltesJatekos();
+        tombFeltoltesGep();
+    }
+    private static void tombKiir() {
+        tombKiirJatekos();
+    }
+
+    private static void tombFeltoltesJatekos() {
+        int felso = 11;
+        int also = 2;
+        for (int i = 0; i < j.length; i++) {
+            j[i] = (int)(Math.random() * (felso - also + 1)) + also;
         }
+    
+    }
+
+    private static void tombFeltoltesGep() {
+       int felso = 11;
+        int also = 2;
+        for (int i = 0; i < g.length; i++) {
+            g[i] = (int)(Math.random() * (felso - also + 1)) + also;
+        } 
+    }
+
+    private static void tombKiirJatekos() {
+        for (int i = 0; i < j.length; i++) {
+            System.out.print(j[i] + " ");
+        }
+        System.out.println("");
     
     }
     
